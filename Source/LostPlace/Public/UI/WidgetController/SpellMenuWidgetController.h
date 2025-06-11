13d9192906ad44ba3d92fc2a74e0df0fe0e271b1
谷静云 <8396256+gu-jingyun@user.noreply.gitee.com>
2025-06-11 22:33:32 +0800

    装备技能UI

---
+++ Source/LostPlace/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LPWidgetController.h"
+#include "SpellMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class LOSTPLACE_API USpellMenuWidgetController : public ULPWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BroadcastInitialValues() override; //广播初始化的值
+	virtual void BindCallbacksToDependencies() override; //绑定数值变动后回调的广播
+};
